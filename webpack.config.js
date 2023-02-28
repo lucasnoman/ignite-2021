e34cf67addbeb2237473dca84f79b2d7fb5c4524
Lucas C. Noman <lucas.noman@outlook.com>
2023-02-28 13:36:36 -0300

    build: start using typescript and configures it

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,13 @@ const isDevelopment = process.env.NODE_ENV !== 'production;';
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map', // permite visualizar no devtools, quando tenho erro, exatamente o mesmo código que tenho no vscode
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx'], // ao passar essas ext, não preciso importar os componentes com extensão
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // ao passar essas ext, não preciso importar os componentes com extensão
   },
   devServer: {
     static: path.resolve(__dirname, 'public'),
@@ -29,7 +29,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader', // integra o babel com o webpack
