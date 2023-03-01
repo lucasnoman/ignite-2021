a414089558eb58081872b2db97d3a801f2679bf1
Lucas C. Noman <lucas.noman@outlook.com>
2023-03-01 15:04:44 -0300

    feat(2nd-class): use post route and save modal form state

--- reactjs/02-dt-money/src/components/NewTransactionModal/index.tsx
+++ reactjs/02-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import { api } from '../../services/api';
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,7 +15,23 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type,
+    };
+
+    api.post('/transactions', data)
+  }
 
   return (
     <Modal
@@ -31,11 +48,21 @@ export function NewTransactionModal({
       >
         <img src={closeImg} alt='Fechar modal' />
       </button>
-      <Container>
+
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
-        <input placeholder='Título' />
-        <input type='number' placeholder='Valor' />
+        <input
+          placeholder='Título'
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          type='number'
+          placeholder='Valor'
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -43,7 +70,7 @@ export function NewTransactionModal({
             onClick={() => setType('deposit')}
             // NOTE - A propriedade isActive é usada pelo Styled Components para estilização
             isActive={type === 'deposit'}
-            activeColor="green"
+            activeColor='green'
           >
             <img src={incomeImg} alt='Entrada' />
             <span>Entrada</span>
@@ -53,14 +80,18 @@ export function NewTransactionModal({
             type='button'
             onClick={() => setType('withdraw')}
             isActive={type === 'withdraw'}
-            activeColor="red"
+            activeColor='red'
           >
             <img src={outcomeImg} alt='Saída' />
             <span>Saída</span>
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder='Categoria' />
+        <input
+          placeholder='Categoria'
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
 
         <button type='submit'>Cadastrar</button>
       </Container>
