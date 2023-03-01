e90e0721cdf3517226e9376caa7f31c3e12d87e3
Lucas C. Noman <lucas.noman@outlook.com>
2023-03-01 12:24:13 -0300

    feat(2nd-class): created app with cra, added "react-modal", "miragejs", "axios" and "styled-components" dependencies

---
+++ reactjs/02-dt-money/src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { GlobalStyle } from './styles/global';
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  // Comunicação de FILHO para PAI no Header.
+  // Dou um nome para a propriedade e envio uma função para ela.
+  // O filho vai conseguir executar essa função e alterar o "state" do pai.
+  return (
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </>
+  );
+}
