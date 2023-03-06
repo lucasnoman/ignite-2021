2c544668e87f59d91040ac5a094c37063f848a70
Lucas C. Noman <lucas.noman@outlook.com>
2023-03-06 10:41:59 -0300

    build(3rd-class): creates and configurates nextjs project

---
+++ reactjs/03-ignews/src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang='pt-br'>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;0,900;1,400&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
