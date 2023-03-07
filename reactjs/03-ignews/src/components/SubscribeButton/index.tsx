027b27177f3e6430b0aad6d6a91516c2dad13e05
Lucas C. Noman <lucas.noman@outlook.com>
2023-03-07 14:18:38 -0300

    feat(3rd-class): use axios to start a checkout on stripe
    This code register the user on stripe and faunadb without duplications. Then proceeds to checkout through stripe

--- reactjs/03-ignews/src/components/SubscribeButton/index.tsx
+++ reactjs/03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { api } from '@/src/services/api';
+import { getStripeJs } from '@/src/services/stripe-js';
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,8 +8,32 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { status } = useSession();
+
+  async function handleSubscribe() {
+    if (status !== 'authenticated') {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe?.redirectToCheckout({sessionId});
+    } catch (error: any) {
+      alert(error.message);
+    }
+  }
+
   return (
-    <button type='button' className={styles.subscribeButton}>
+    <button
+      type='button'
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
